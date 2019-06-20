6896a789b58cf1b3d2356846f2bf960c41eb7513
Nico <if17b023@technikum-wien.at>
2019-06-20 16:53:18 +0200

    redraw glitch

---
+++ first_thing/first_thing/redraw_glitch.c
@@ -0,0 +1,50 @@
+#include "redraw_glitch.h"
+
+
+float hour = 0.0;
+float day = 0.0;
+float inc = 20.00;
+
+bool checkIfInRedrawGlitchRoom(int x, int y) {
+	if (-10 < x && x < 10 && -30 < y && y < -10) {
+		return true;
+	}
+	return false;
+}
+
+void drawRedrawGlitch() {
+
+	hour += inc;
+	day += inc / 24.0;
+	hour = hour - ((int)(hour / 24)) * 24;
+	day = day - ((int)(day / 365)) * 365;
+
+	glTranslatef(-6.0, 0.0, 4.0);
+	glScalef(.2, .2, .2);
+	glRotatef(360 * day / 365.0, 0.0, 1.0, 0.0);
+
+	// ecliptic
+	glRotatef(15.0, 1.0, 0.0, 0.0);
+
+	// sun
+	glColor3f(1.0, 1.0, 0.0);
+	glutSolidSphere(1.0, 15, 15);
+
+	// earth
+	// position around the sun
+	glRotatef(360.0*day / 365.0, 0.0, 1.0, 0.0);
+	glTranslatef(4.0, 0.0, 0.0);
+
+	glPushMatrix();
+	// rotate the earth on its axis
+	glRotatef(360.0*hour / 24.0, 0.0, 1.0, 0.0);
+	glColor3f(1.0, 1.0, 1.0);
+	glutSolidSphere(0.4, 10, 10);
+	glPopMatrix();
+
+	// moon
+	glRotatef(360.0 * 4 * day / 365.0, 0.0, 1.0, 0.0);
+	glTranslatef(0.7f, 0.0f, 0.0f);
+	glColor3f(0.3f, 0.7f, 0.3f);
+	glutSolidSphere(0.1f, 10, 10);
+}
---
+++ first_thing/first_thing/redraw_glitch.h
@@ -0,0 +1,9 @@
+#include <GL/glut.h> 
+#include <GL/gl.h>  
+#include <GL/glu.h>  
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void drawRedrawGlitch();
+bool checkIfInRedrawGlitchRoom(int x, int y);

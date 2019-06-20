4a18e0358bdee08182a106f998a34ca64c4bbd82
SlightlyObscure <33038788+SlightlyObscure@users.noreply.github.com>
2019-06-20 18:42:12 +0200

    small fix to make bug buggy

--- first_thing/first_thing/art.c
+++ first_thing/first_thing/art.c
@@ -1,16 +1,16 @@
 #include "stdafx.h"
 
-void drawPainting(GLuint painting) {
+void drawPainting(GLuint painting, float move) {
 	glEnable(GL_TEXTURE_2D);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 	glBindTexture(GL_TEXTURE_2D, painting);
 
 	glBegin(GL_QUADS);
 
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-2, -3, 0);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(2, -3, 0);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(2, 2, 0);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-2, 2, 0);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-2 + move, -3, 0);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(2 + move, -3, 0);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(2 + move, 2, 0);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-2 + move, 2, 0);
 
 	glEnd();
 
@@ -22,11 +22,11 @@ void makeArt(GLuint painting1, GLuint painting2, GLuint painting3) {
 
 	glTranslatef(0, 0, 18.99f);
 
-	drawPainting(painting1);
+	drawPainting(painting1, 0.00f);
 	glutPostRedisplay();
-	drawPainting(painting2);
+	drawPainting(painting2, 0.01f);
 	glutPostRedisplay();
-	drawPainting(painting3);
+	drawPainting(painting3, -0.01f);
 
 	glPopMatrix();
 }
